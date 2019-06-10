9b67a3c81e3216f6a2a8699532bff827109fc22c
Koki Ibukuro <koki.ibukuro@gmail..com>
2019-06-10 16:49:08 +0200

    Make border

--- Assets/PoissonBlending/PoissonBlendingCuda.cs
+++ Assets/PoissonBlending/PoissonBlendingCuda.cs
@@ -14,8 +14,9 @@ namespace PoissonBlending
         [SerializeField] Texture2D target = null;
         [SerializeField] ComputeShader compute = null;
 
-        RenderTexture tex;
-        int kernelMask;
+        RenderTexture result;
+        RenderTexture border;
+        int kernelBorder;
         int kernelBlend;
         int3 tGroups;
 
@@ -24,11 +25,15 @@ namespace PoissonBlending
             Debug.Assert(source.width == mask.width && source.width == target.width);
             Debug.Assert(source.height == mask.height && source.height == target.height);
 
-            tex = new RenderTexture(source.width, source.height, 0);
-            tex.enableRandomWrite = true;
-            tex.Create();
+            result = new RenderTexture(source.width, source.height, 0);
+            result.enableRandomWrite = true;
+            result.Create();
 
-            kernelMask = compute.FindKernel("MakeMask");
+            border = new RenderTexture(source.width, source.height, 0);
+            border.enableRandomWrite = true;
+            border.Create();
+
+            kernelBorder = compute.FindKernel("MakeBorder");
             kernelBlend = compute.FindKernel("PoissonBlending");
             uint3 threads = compute.GetThreadGroupSize(kernelBlend);
             Debug.Assert(source.width % threads.x == 0);
@@ -42,33 +47,35 @@ namespace PoissonBlending
             int w = Screen.width / 2;
             int h = Screen.height / 2;
 
-            GUI.DrawTexture(new Rect(0, 0, w, h), source);
+            GUI.DrawTexture(new Rect(0, 0, w, h), border);
             GUI.DrawTexture(new Rect(w, 0, w, h), mask);
             GUI.DrawTexture(new Rect(0, h, w, h), target);
-            GUI.DrawTexture(new Rect(w, h, w, h), tex);
+            GUI.DrawTexture(new Rect(w, h, w, h), result);
         }
 
         void Update()
         {
             var sw = Stopwatch.StartNew();
 
-
-            compute.Dispatch(kernelMask, tGroups.x, tGroups.y, tGroups.z);
+            compute.SetTexture(kernelBorder, "Mask", mask);
+            compute.SetTexture(kernelBorder, "Border", border);
+            compute.Dispatch(kernelBorder, tGroups.x, tGroups.y, tGroups.z);
 
             compute.SetTexture(kernelBlend, "Source", source);
             compute.SetTexture(kernelBlend, "Mask", mask);
             compute.SetTexture(kernelBlend, "Target", target);
-            compute.SetTexture(kernelBlend, "Result", tex);
+            compute.SetTexture(kernelBlend, "Result", result);
             compute.Dispatch(kernelBlend, tGroups.x, tGroups.y, tGroups.z);
 
             sw.Stop();
-            double d = (double)sw.ElapsedTicks / (double)TimeSpan.TicksPerMillisecond;
+            double d = (double)sw.ElapsedTicks / TimeSpan.TicksPerMillisecond;
             Debug.Log($"Cuda: {d} ms");
         }
 
         void OnDestroy()
         {
-            tex.Release();
+            result.Release();
+            border.Release();
         }
     }
 }
